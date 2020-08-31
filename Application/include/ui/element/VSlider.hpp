b1412adb2e0f0965469bb93bae84309174a8ece2
tallbl0nde <40382856+tallbl0nde@users.noreply.github.com>
2020-08-31 21:17:29 +0930

    App: Add vertical slider

---
+++ Application/include/ui/element/VSlider.hpp
@@ -0,0 +1,42 @@
+#ifndef ELEMENT_VSLIDER_HPP
+#define ELEMENT_VSLIDER_HPP
+
+#include "Aether/Aether.hpp"
+
+namespace CustomElm {
+    class VSlider : public Aether::Element {
+        private:
+            Aether::Rectangle * barBg;      // Background bar
+            Aether::Rectangle * barFg;      // Forground bar
+            Aether::Ellipse * knob;         // Knob
+            float nudge;                    // Amount to move when button pressed
+            float value_;                   // Value/position
+
+        public:
+            // X, Y, W, H, knob diameter
+            VSlider(int, int, int, int, int);
+
+            // Override to handle button events
+            bool handleEvent(Aether::InputEvent *);
+            void render();
+
+            // Set/get value (0.0 - 100.0)
+            float value();
+            void setValue(float);
+
+            // Set amount to inc/dec on button press
+            void setNudge(float);
+
+            // Set colours
+            void setBarBackgroundColour(const Aether::Colour &);
+            void setBarForegroundColour(const Aether::Colour &);
+            void setKnobColour(const Aether::Colour &);
+
+            // Override to only highlight the knob
+            SDL_Texture * renderHighlightBG();
+            SDL_Texture * renderHighlight();
+            SDL_Texture * renderSelection();
+    };
+};
+
+#endif
--- Application/source/ui/element/VSlider.cpp
+++ Application/source/ui/element/VSlider.cpp
@@ -1,54 +1,46 @@
-#include "ui/element/Slider.hpp"
+#include "ui/element/VSlider.hpp"
 
 // Amount of pixels either side of bar to accept as touch
-#define LEEWAY 10
+#define LEEWAY 2
 
 namespace CustomElm {
-    Slider::Slider(int x, int y, int w, int h, int bh) : Aether::Element(x, y, w, h) {
-        this->bar = new Aether::RoundProgressBar(x + h/2, y + (h - bh)/2, w - h, bh);
-        this->addElement(this->bar);
-        this->knob = new Aether::Ellipse(x, y, h);
+    VSlider::VSlider(int x, int y, int w, int h, int bw) : Aether::Element(x, y, w, h) {
+        this->barBg = new Aether::Rectangle(x + (w - bw)/2, y + w/2, bw, h - w, bw/2 - 1);
+        this->addElement(this->barBg);
+        this->barFg = new Aether::Rectangle(x + (w - bw)/2, y + w/2, bw, h - w, bw/2 - 1);
+        this->addElement(this->barFg);
+        this->knob = new Aether::Ellipse(x, y, w);
+        this->addElement(this->knob);
         this->setNudge(1);
         this->setValue(0.0);
-        this->addElement(this->knob);
         this->setSelectable(true);
         this->setTouchable(true);
     }
 
     // Similar to Element::handleEvent but we have our own behaviour
-    bool Slider::handleEvent(Aether::InputEvent * e) {
+    bool VSlider::handleEvent(Aether::InputEvent * e) {
         bool b = false;
 
         // If it's a touch event jump to position
         if (e->type() == Aether::EventType::TouchPressed) {
             if (e->touchX() >= (this->x() - LEEWAY) && e->touchY() >= (this->y() - LEEWAY) && e->touchX() <= this->x() + this->w() + LEEWAY && e->touchY() <= this->y() + this->h() + LEEWAY && !this->hidden()) {
                 this->setSelected(true);
-                float val = 100.0 * (e->touchX() - this->x())/(float)this->w();
-                if (val < 0.0) {
-                    val = 0.0;
-                } else if (val > 100.0) {
-                    val = 100.0;
-                }
+                float val = 100.0 * ((this->y() + this->h()) - e->touchY())/(float)this->h();
                 this->setValue(val);
                 b = true;
             }
 
         } else if (e->type() == Aether::EventType::TouchMoved && this->selected()) {
-            float val = 100.0 * (e->touchX() - this->x())/(float)this->w();
-            if (val < 0.0) {
-                val = 0.0;
-            } else if (val > 100.0) {
-                val = 100.0;
-            }
+            float val = 100.0 * ((this->y() + this->h()) - e->touchY())/(float)this->h();
             this->setValue(val);
             b = true;
 
         // Leave new value on release
         } else if (e->type() == Aether::EventType::TouchReleased && this->selected()) {
-            if (e->touchX() < this->x()) {
-                this->setValue(0.0);
-            } else if (e->touchX() > this->x() + this->w()) {
+            if (e->touchY() < this->y()) {
                 this->setValue(100.0);
+            } else if (e->touchY() > this->y() + this->h()) {
+                this->setValue(0.0);
             }
             this->setSelected(false);
             moveHighlight(this);
@@ -62,17 +54,12 @@ namespace CustomElm {
             if (e->button() == Aether::Button::A && this->highlighted()) {
                 this->setSelected(true);
                 b = true;
-            }
-
-            // Otherwise move in direction
-            if (this->selected()) {
-                if (e->button() == Aether::Button::DPAD_RIGHT) {
-                    this->setValue(this->value() + this->nudge);
-                    b = true;
-                } else if (e->button() == Aether::Button::DPAD_LEFT) {
-                    this->setValue(this->value() - this->nudge);
-                    b = true;
-                }
+            } else if (e->button() == Aether::Button::DPAD_UP) {
+                this->setValue(this->value() + this->nudge);
+                b = true;
+            } else if (e->button() == Aether::Button::DPAD_DOWN) {
+                this->setValue(this->value() - this->nudge);
+                b = true;
             }
 
         } else if (e->type() == Aether::EventType::ButtonReleased) {
@@ -90,7 +77,7 @@ namespace CustomElm {
     }
 
     // Need to override this in order to draw highlight on top of children
-    void Slider::render() {
+    void VSlider::render() {
         // Do nothing if hidden or off-screen
         if (!this->isVisible()) {
             return;
@@ -117,60 +104,48 @@ namespace CustomElm {
         }
     }
 
-    float Slider::value() {
-        return this->bar->value();
+    float VSlider::value() {
+        return this->value_;
     }
 
-    void Slider::setValue(float val) {
-        this->bar->setValue(val);
-        this->knob->setX(this->x() + ((this->w() - this->knob->xDiameter()) * (this->bar->value()/100.0)) - 3);
+    void VSlider::setValue(float val) {
+        float old = this->value_;
+        this->value_ = (val < 0.0 ? 0.0 : (val > 100.0 ? 100.0 : val));
+        if (old != this->value_) {
+            int h = this->barFg->texH() * (this->value_/100.0);
+            this->barFg->setY(this->barBg->y() + this->barBg->h() - h);
+            this->barFg->setH(h);
+            this->barFg->setMask(0, this->barFg->texH() - h, this->barFg->texW(), h);
+        }
+        this->knob->setY(this->barBg->y() + this->barBg->h() - ((this->value_/100.0) * this->barBg->h()) - this->knob->yDiameter()/2);
     }
 
-    void Slider::setNudge(float n) {
+    void VSlider::setNudge(float n) {
         this->nudge = n;
     }
 
-    void Slider::setBarBackgroundColour(Aether::Colour c) {
-        this->bar->setBackgroundColour(c);
+    void VSlider::setBarBackgroundColour(const Aether::Colour & c) {
+        this->barBg->setColour(c);
     }
 
-    void Slider::setBarForegroundColour(Aether::Colour c) {
-        this->bar->setForegroundColour(c);
+    void VSlider::setBarForegroundColour(const Aether::Colour & c) {
+        this->barFg->setColour(c);
     }
 
-    void Slider::setKnobColour(Aether::Colour c) {
+    void VSlider::setKnobColour(const Aether::Colour & c) {
         this->knob->setColour(c);
     }
 
-    void Slider::setW(int w) {
-        Element::setW(w);
-        this->bar->setW(this->w() - this->knob->xDiameter());
-        this->bar->setX(this->x() + this->knob->xDiameter()/2);
-        this->knob->setX(this->x() + ((this->w() - this->knob->xDiameter()) * (this->bar->value()/100.0)) - 3);
-    }
-
-    void Slider::setH(int h) {
-        Element::setH(h);
-        this->knob->setXDiameter(h);
-        this->knob->setYDiameter(h);
-        this->bar->setY(this->y() + (this->h() - this->bar->h())/2);
-    }
-
-    void Slider::setBarH(int bh) {
-        this->bar->setH(bh);
-        this->bar->setY(this->y() + (this->h() - this->bar->h())/2);
-    }
-
-    SDL_Texture * Slider::renderHighlightBG() {
+    SDL_Texture * VSlider::renderHighlightBG() {
         // No background needed
         return nullptr;
     }
 
-    SDL_Texture * Slider::renderHighlight() {
+    SDL_Texture * VSlider::renderHighlight() {
         return SDLHelper::renderEllipse(this->knob->xDiameter()/2, this->knob->yDiameter()/2, this->hiSize);
     }
 
-    SDL_Texture * Slider::renderSelection() {
+    SDL_Texture * VSlider::renderSelection() {
         return SDLHelper::renderFilledEllipse(this->knob->xDiameter()/2, this->knob->yDiameter()/2);
     }
 };
